ded2d5898f961fe511f8cfb8540ff7e76cc9d67a
Gowri Murthy <72099614+gowri-m@users.noreply.github.com>
2022-03-28 00:09:50 -0400

    Delete src directory

--- src/RhymeWord.js
+++
@@ -1,7 +0,0 @@
-function RhymeWord(props) {
-    return(<div>{props.sylVal ? <h2>{props.sylVal}</h2> : ""}
-            <li key={props.keyVal}>{props.description} <button onClick={props.onClickSaveWords}>(save)</button></li>
-            </div>) 
-}
-
-export default RhymeWord;
